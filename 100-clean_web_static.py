de199da479498ac0f818272faffd8b904d205d10
zibxto <christianaziba@gmail.com>
2023-12-11 17:50:20 +0100

    update fil

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,50 +3,53 @@
 Fabric script (based on the file 3-deploy_web_static.py)
 that deletes out-of-date archives, using the function do_clean
 """
-from fabric.api import local, env, put, run
-from time import strftime
-import os.path
+from fabric.api import *
+from datetime import datetime
+import os
+
 env.hosts = ['100.25.15.14', '34.232.78.53']
 env.user = "ubuntu"
 
 
 def do_pack():
-    """generate .tgz archive of web_static/ folder"""
-    timenow = strftime("%Y%M%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        filename = "versions/web_static_{}.tgz".format(timenow)
-        local("tar -cvzf {} web_static/".format(filename))
-        return filename
-    except Exception:
+    """generates a .tgz archive from the contents of a file"""
+    date = datetime.now()
+    date = date.strftime("%Y%m%d%H%M%S")
+    archive_path = "./versions/web_static_{}.tgz".format(date)
+    if not os.path.exists("./versions/"):
+        os.mkdir("./versions/")
+    cmd = local("tar -cvzf {} web_static/".format(archive_path))
+
+    if cmd.succeeded:
+        return archive_path
+    if cmd.failed:
         return None
 
 
 def do_deploy(archive_path):
-    """
-    Deploy archive to web server
-    """
-    if os.path.isfile(archive_path) is False:
-        return False
+    """distributes an archive to web servers"""
     try:
-        filename = archive_path.split("/")[-1]
-        no_ext = filename.split(".")[0]
-        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
-        symlink = "/data/web_static/current"
+        if not os.path.exists(archive_path):
+            return False
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_no_ext))
-        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
-        run("rm -rf {}web_static".format(path_no_ext))
-        run("rm -rf {}".format(symlink))
-        run("ln -s {} {}".format(path_no_ext, symlink))
+        archive_filename = archive_path.split('/')[-1]
+        uncompressed_filename = archive_filename[:-4]
+        run("mkdir -p /data/web_static/releases/{}"
+            .format(uncompressed_filename))
+        run("tar -xvzf  /tmp/{} -C /data/web_static/releases/{}"
+            .format(archive_filename, uncompressed_filename))
+        run("sudo rm /tmp/{}".format(archive_filename))
+        run("sudo rm -r /data/web_static/current")
+        run("ln -s\
+             /data/web_static/releases/{}/web_static /data/web_static/current"
+            .format(uncompressed_filename))
         return True
     except Exception:
         return False
 
 
 def deploy():
+    """calls do_pack() and do_deploy"""
     archive_path = do_pack()
     if archive_path is None:
         return False
