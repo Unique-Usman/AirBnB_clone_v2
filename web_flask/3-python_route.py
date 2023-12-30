cf52def2cc0fdb76880fac88ddd4c9466f48ad78
zibxto <christianaziba@gmail.com>
2023-12-30 17:18:51 +0100

    add file

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """hbnb route definition"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """hbnb route definition"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """c route definition"""
+    return f"C {text}".replace("_", " ")
+
+
+@app.route('/python')
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """python route definition"""
+    return f"Python {text}".replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
