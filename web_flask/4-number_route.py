d6b652345ba12b6a6b7e1c97e258dca1d6c7c30e
zibxto <christianaziba@gmail.com>
2023-12-31 14:43:24 +0100

    add file

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """home route definition"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """hbnb route definition"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """c route definition"""
+    return f"C {text}".replace("_", " ")
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """python route definition"""
+    return f"Python {text}".replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """number route definition"""
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
